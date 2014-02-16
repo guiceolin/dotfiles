8677ea361f16b72224546f46f88343b7336cbcac
Guilherme Ceolin <guiceolin@gmail.com>
2014-02-16 14:58:55 -0300

    new bash implementation

---
+++ lib/lib.sh
@@ -0,0 +1,31 @@
+function __current_dir ()
+{
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  printf "${DIR}"
+}
+
+function __modules_dir ()
+{
+  printf $(__current_dir)/../modules_enabled/
+}
+
+function __load_modules ()
+{
+  MODULES=$(__current_dir)/../modules_enabled/**/*_module
+  for m in $MODULES
+  do
+    CURRENT_MODULE="$( dirname $m )"
+    source $m
+  done
+}
+function __load_modules_path ()
+{
+  PATHS=$(__current_dir)/../modules_enabled/**/PATH
+  for m in $PATHS
+  do
+    CURRENT_MODULE="$( dirname $m )"
+    source $m
+  done
+}
