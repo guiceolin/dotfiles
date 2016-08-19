8865a0589f2d8f719ec0bd0f2609c54e2d39009f
Guilherme Ceolin <guiceolin@gmail.com>
2016-08-19 11:31:24 -0300

    Add step to install homebrew

--- macos/brew.sh
+++ macos/brew.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 #
 # Make sure we’re using the latest Homebrew.
 brew update
