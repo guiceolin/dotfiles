4d1ba822a74a551b644de787149fb175878570eb
Guilherme Ceolin <guiceolin@gmail.com>
2018-07-24 11:29:39 -0300

    change virtualenv folder to .venv

--- plugins/python/virtualenv.plugin.zsh
+++ plugins/python/virtualenv.plugin.zsh
@@ -2,7 +2,7 @@ virtualenvswitcher:current_dir_has_virtualenv_folder(){
     echo "$(pwd)" | grep "$(dirname "$VIRTUAL_ENV")" > /dev/null 2>&1
 }
 virtualenvswitcher:source() {
-    local DOTPYVENV_DIR=".env"
+    local DOTPYVENV_DIR=".venv"
 
     if [ -n "$VIRTUAL_ENV"  ]; then
         virtualenvswitcher:current_dir_has_virtualenv_folder || deactivate
