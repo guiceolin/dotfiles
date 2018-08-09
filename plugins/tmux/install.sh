67181a0fe8768efd63294ae564b8e5f6c8140413
Guilherme Ceolin <guiceolin@gmail.com>
2018-08-09 18:42:01 -0300

    Tweak tmux

--- plugins/tmux/install.sh
+++ plugins/tmux/install.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# Install Tmux
+
+rm -fr /tmp/tmux
+git clone https://github.com/tmux/tmux.git /tmp/tmux
+cd /tmp/tmux
+sh autogen.sh
+./configure && make
+cp tmux ~/.local/bin
+cd -
+rm -fr /tmp/tmux
+
+# Install tmux.conf
+
 if [[ -f ~/.tmux.conf ]]; then
   mv ~/.tmux.conf ~/.tmux.conf.old
   ln -s $(realpath ".tmux.conf") $HOME
