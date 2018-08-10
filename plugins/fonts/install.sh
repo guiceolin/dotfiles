71e019a4c913b28fb8ca26bf4297d534c1af1027
Guilherme Ceolin <guiceolin@gmail.com>
2018-08-10 13:52:33 -0300

    New font install plugin

--- plugins/fonts/install.sh
+++ plugins/fonts/install.sh
@@ -1,7 +1,15 @@
+#!/usr/bin/env bash
 # Install Mononoki font
-mkdir -p /tmp/mononoki && pushd /tmp/mononoki
+mkdir /tmp/mononoki && cd /tmp/mononoki
+
 LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/ryanoasis/nerd-fonts/releases/latest|jq '.tag_name'|sed 's/"//g')
 wget "https://github.com/ryanoasis/nerd-fonts/releases/download/$LATEST_VERSION/mononoki.zip"
+
 unzip mononoki.zip
-mv *.ttf ~/Library/Fonts/
-rm -rf /tmp/mononoki && popd
+# TODO Check OS before cp fonts
+mkdir -p ~/Library/Fonts/
+cp ./*.ttf ~/Library/Fonts/
+mkdir -p ~/.local/share/fonts/
+cp ./*.ttf ~/.local/share/fonts/
+
+cd - && rm -rf /tmp/mononoki
