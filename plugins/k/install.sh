af87bd7b4189b7b2442e9540979a1ff31d2d3e90
Guilherme Ceolin <guiceolin@gmail.com>
2018-12-17 12:45:10 -0200

    Use https instead git protocol to clone K

--- plugins/k/install.sh
+++ plugins/k/install.sh
@@ -3,5 +3,5 @@ if [[ $? != 0 ]] ; then
   echo -n '> To install k plugin, you need git.'
   echo -n '> Skipping k plugin install.'
 else
-  git clone git@github.com:supercrabtree/k.git
+  git clone https://github.com/supercrabtree/k.git
 fi
