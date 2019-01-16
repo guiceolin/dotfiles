9e45c370659355a693749a044f15a0a3abea6b94
Guilherme Ceolin <guiceolin@gmail.com>
2019-01-16 17:01:21 -0200

    Add nvm plugin

---
+++ plugins/nvm/install.sh
@@ -0,0 +1,4 @@
+export NVM_DIR="$HOME/.nvm"
+git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+cd "$NVM_DIR"
+git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
