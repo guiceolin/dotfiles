48fe62c0559857842ff59a286c41420f1420dcb1
Guilherme Ceolin <guiceolin@gmail.com>
2019-01-17 13:36:05 -0200

    Only init pyenv if not initialized yet

--- plugins/pyenv/pyenv.plugin.zsh
+++ plugins/pyenv/pyenv.plugin.zsh
@@ -1,4 +1,7 @@
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+# Only init pyenv if not initialized yet
+if [ -z "$PYENV_ROOT" ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
 
-eval "$(pyenv init -)"
+  eval "$(pyenv init -)"
+fi
