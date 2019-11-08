faddc9323e5ec0ba66eda72d7158b7768da0ad98
Guilherme Ceolin <guiceolin@gmail.com>
2019-11-08 11:32:05 -0300

    Add support to pacman dependencies

--- lib/dependencies.zsh
+++ lib/dependencies.zsh
@@ -1,19 +1,28 @@
 dotfiles:dependencies:install(){
-  # Install Homebrew + dependencies
-  which -s brew
-  if [[ $? != 0 ]] ; then
-    dotfiles:dependencies:install_manager
+
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sudo pacman -Syyu
+    sudo pacman -S --needed - < pkglist.txt
+
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+
+    which -s brew
+    if [[ $? != 0 ]] ; then
+      echo -n "> Brew not found. Installing..."
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      echo " done!"
+    else
+      echo "> Homebrew already installed, skipping."
+    fi
+
+    echo -n "> Installing brew dependencies..."
+    brew bundle install &> /dev/null
+    echo " done!"
+
   else
-    echo "> Homebrew already installed, skipping."
+          echo "> Unknown OS, aborting..."
+          return 1
   fi
 
-  echo -n "> Installing brew dependencies..."
-  brew bundle install &> /dev/null
-  echo " done!"
-}
-
-dotfiles:dependencies:install_manager(){
-  echo -n "> Brew not found. Installing..."
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  echo " done!"
-}
+  # Install Homebrew + dependencies
+  }
