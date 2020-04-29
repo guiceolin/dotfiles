d134fa75c86e09559617cd572b5922ead9ea2d2e
Guilherme Ceolin <guiceolin@gmail.com>
2020-04-29 10:25:55 -0300

    if bat is present, use it to display man

--- plugins/zsh/alias.plugin.zsh
+++ plugins/zsh/alias.plugin.zsh
@@ -6,6 +6,11 @@ bindkey '^[[B' history-substring-search-down
 alias grep="grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}"
 
 alias ls="exa"
-alias cat="bat"
+
+if [ -x "$(command -v bat)" ]
+then
+  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+  alias cat="bat"
+fi
 
 export FZF_DEFAULT_OPTS="--ansi --preview-window 'right:60%' --preview 'bat --color=always --style=header,grid --line-range :300 {}'"
