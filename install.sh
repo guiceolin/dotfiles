3f1ff402a6c7c6080828f0f6976d78999b8d46bf
Guilherme Ceolin <guiceolin@Hermes.local>
2021-03-23 19:42:07 -0300

    use zsh to install

--- install.sh
+++ install.sh
@@ -1,20 +1,4 @@
-#!/bin/bash
-
-source lib/plugins.zsh
-source lib/dependencies.zsh
-
-dotfiles:dependencies:install
-
-dotfiles:plugins:install_plugins
-
-remote_plugins=(
-  https://github.com/zsh-users/zsh-syntax-highlighting.git
-  https://github.com/zsh-users/zsh-autosuggestions.git
-  https://github.com/zsh-users/zsh-history-substring-search.git
-  https://github.com/zsh-users/zsh-completions.git
-)
-
-dotfiles:plugins:install_remote_plugins
+#!/usr/bin/env zsh
 
 export DOTFILES_HOME=$HOME/dotfiles
 
