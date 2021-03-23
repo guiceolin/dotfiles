01336523d24d7bb25c824d2ecf0f09a8c520ecdf
Guilherme Ceolin <guiceolin@Hermes.local>
2021-03-23 19:01:06 -0300

    More generic linking

--- install.sh
+++ install.sh
@@ -16,20 +16,18 @@ remote_plugins=(
 
 dotfiles:plugins:install_remote_plugins
 
+export DOTFILES_HOME=$HOME/dotfiles
+
 # Link zshrc to $HOME
 if [[ -f ~/.zshrc ]]; then
   mv ~/.zshrc ~/.zshrc.old
-  ln -s $(realpath "zshrc") $HOME/.zshrc
-else
-  ln -sf $(realpath "zshrc") $HOME/.zshrc
 fi
+ln -sf $DOTFILES_HOME/zshrc $HOME/.zshrc
 
 # Link zprofile to $HOME
 if [[ -f ~/.zprofile ]]; then
   mv ~/.zprofile ~/.zprofile.old
-  ln -s $(realpath "zprofile") $HOME/.zprofile
-else
-  ln -sf $(realpath "zprofile") $HOME/.zprofile
 fi
+ln -sf $DOTFILES_HOME/zprofile $HOME/.zprofile
 
 mkdir -p $HOME/.local/bin
