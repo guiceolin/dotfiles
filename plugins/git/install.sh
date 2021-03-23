a2bab431e4ed14c0bb7b3293fd8d965cbdf9eae0
Guilherme Ceolin <guiceolin@gmail.com>
2021-03-23 20:05:20 -0300

    (git) fix git install

--- plugins/git/install.sh
+++ plugins/git/install.sh
@@ -3,7 +3,6 @@
 mkdir -p ~/.config
 if [[ -f ~/.config/git ]]; then
   mv ~/.config/git ~/.config/git.old
-  ln -s $(realpath ".config/git") ~/.config
-else
-  ln -sf $(realpath ".config/git") ~/.config
 fi
+
+ln -sf $DOTFILES/plugins/git/.config/git ~/.config
