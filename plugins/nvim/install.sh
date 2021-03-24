b9d81a13a195dd65979ab4385054f913a6aa6aec
Guilherme Ceolin <guiceolin@gmail.com>
2021-03-24 20:01:21 -0300

    (nvim) move old nvim config to bkp

--- plugins/nvim/install.sh
+++ plugins/nvim/install.sh
@@ -3,23 +3,24 @@
 mkdir -p ~/.config
 if [[ -f ~/.config/nvim ]]; then
   mv ~/.config/nvim ~/.config/nvim.old
-  ln -s $(realpath "config/nvim") ~/.config
-else
-  ln -sf $(realpath "config/nvim") ~/.config
 fi
+ln -sf $DOTFILES/plugins/nvim/.config/nvim ~/.config
 
-# Install python 3
-pyenv install -s 3.6.6
-pyenv global 3.6.6
+# # Install python 3
+# pyenv install -s 3.6.6
+# pyenv global 3.6.6
+# 
+# pip install --upgrade pip
+# pip install virtualenv
+# 
+# NVIM_VIRTUALENV=~/.local/.venv
+# 
+# mkdir -p $NVIM_VIRTUALENV
+# virtualenv $NVIM_VIRTUALENV
+# 
+# $NVIM_VIRTUALENV/bin/pip install neovim flake8 black
+# 
+# ln -s $HOME/.local/.venv/bin/flake8 ~/.local/bin/flake8
 
-pip install --upgrade pip
-pip install virtualenv
-
-NVIM_VIRTUALENV=~/.local/.venv
-
-mkdir -p $NVIM_VIRTUALENV
-virtualenv $NVIM_VIRTUALENV
-
-$NVIM_VIRTUALENV/bin/pip install neovim flake8 black
-
-ln -s $HOME/.local/.venv/bin/flake8 ~/.local/bin/flake8
+git clone https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
