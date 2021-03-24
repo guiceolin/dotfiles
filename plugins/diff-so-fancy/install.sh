c7b0d8d7fa5d1bcdf55ecfbc583073af14b74019
Guilherme Ceolin <guiceolin@gmail.com>
2021-03-23 22:35:56 -0300

    merge git and diff-so-fancy

--- plugins/diff-so-fancy/install.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p bin
-curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy > bin/diff-so-fancy
-chmod +x bin/diff-so-fancy
-ln -sf $(realpath bin/diff-so-fancy) ~/.local/bin/
--- plugins/git/install.sh
+++ plugins/git/install.sh
@@ -1,8 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh 
 
-mkdir -p ~/.config
-if [[ -f ~/.config/git ]]; then
-  mv ~/.config/git ~/.config/git.old
-fi
+set -x
 
 ln -sf $DOTFILES/plugins/git/.config/git ~/.config
+mkdir -p $DOTFILES/plugins/git/bin
+pushd $DOTFILES/plugins/git/bin
+curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy > diff-so-fancy
+chmod +x diff-so-fancy
+popd
+
+ln -sf $DOTFILES/plugins/git/bin/diff-so-fancy ~/.local/bin/
