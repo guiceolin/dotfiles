a81f2ede281acd2d74e712c54808eba8bb2a35fc
Guilherme Ceolin <guiceolin@gmail.com>
2021-03-26 21:39:25 -0300

    (nvim) install java lsp

--- plugins/nvim/install.sh
+++ plugins/nvim/install.sh
@@ -22,21 +22,38 @@ ln -sf $DOTFILES/plugins/nvim/.config/nvim ~/.config
 # 
 # ln -s $HOME/.local/.venv/bin/flake8 ~/.local/bin/flake8
 
-git clone https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+PACKER_PATH=~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+if [ ! -d $PACKER_PATH ]; then
+  git clone https://github.com/wbthomason/packer.nvim $PACKER_PATH
+fi
 
 # Install LSPs
 
 mkdir -p $DOTFILES/plugins/nvim/lsp
 
 pushd $DOTFILES/plugins/nvim/lsp
-git clone https://github.com/sumneko/lua-language-server
-cd lua-language-server
-git submodule update --init --recursive
-cd 3rd/luamake
-ninja -f ninja/macos.ninja
-cd ../..
-./3rd/luamake/luamake rebuild
-cd ..
+
+# Lua LSP
+if [ ! -d "./lua-language-server" ]; then
+  git clone https://github.com/sumneko/lua-language-server
+  cd lua-language-server
+  git submodule update --init --recursive
+  cd 3rd/luamake
+  ninja -f ninja/macos.ninja
+  cd ../..
+  ./3rd/luamake/luamake rebuild
+  cd ..
+fi
+
+# Java JDTLS
+if [ ! -d './eclipse.jdt.ls' ]; then
+  git clone https://github.com/eclipse/eclipse.jdt.ls.git
+  cd eclipse.jdt.ls
+  ./mvnw clean verify
+  cd ..
+fi
+ln -s $DOTFILES/plugins/nvim/bin/java-lsp $HOME/.local/bin
+
 popd
 
