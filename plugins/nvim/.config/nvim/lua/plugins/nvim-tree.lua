1b32e0eb11a769910610ef1001feb499fb728cff
Guilherme Ceolin <guiceolin@gmail.com>
2021-03-29 22:30:42 -0300

    (nvim) show dotfiles on nvim-tree by default

--- plugins/nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ plugins/nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -8,7 +8,7 @@ vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_hide_dotfiles = 1
+vim.g.nvim_tree_hide_dotfiles = 0
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_allow_resize = 1
 
