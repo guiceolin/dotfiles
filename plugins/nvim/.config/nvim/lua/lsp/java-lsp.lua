e267aa23648267cdf48559925cb096f322afd331
Guilherme Ceolin <guiceolin@gmail.com>
2021-03-29 23:01:19 -0300

    (nvim) use default mappings on java lsp

--- plugins/nvim/.config/nvim/lua/lsp/java-lsp.lua
+++ plugins/nvim/.config/nvim/lua/lsp/java-lsp.lua
@@ -2,6 +2,21 @@ vim.api.nvim_exec(
 [[
   augroup lsp
     au!
-    au FileType java lua require('jdtls').start_or_attach({cmd = {'java-lsp'}})
+    au FileType java lua require('lsp.java-lsp').setup() 
   augroup end
 ]], false)
+
+local M = {}
+
+function M.setup()
+  local config = {}
+
+  config.on_attach = function(client, buffnr)
+    require'lsp.keymap'.set_keymaps(client, buffnr)
+  end
+
+  config.cmd = { 'java-lsp' }
+  require('jdtls').start_or_attach(config)
+end
+
+return M
