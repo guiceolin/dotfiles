8f9434f6f8c077777c7c0456727533db1ad21afb
Guilherme Ceolin <guiceolin@gmail.com>
2021-04-28 21:37:26 -0300

    (nvim) configure java lsp

--- plugins/nvim/.config/nvim/lua/lsp/java-lsp.lua
+++ plugins/nvim/.config/nvim/lua/lsp/java-lsp.lua
@@ -9,13 +9,48 @@ vim.api.nvim_exec(
 local M = {}
 
 function M.setup()
-  local config = {}
+  local root_markers = {'gradlew', 'pom.xml'}
+  local root_dir = require('jdtls.setup').find_root(root_markers)
+  local home = os.getenv('HOME')
 
+  local capabilities = {
+    workspace = {
+      configuration = true
+    },
+    textDocument = {
+      completion = {
+        completionItem = {
+          snippetSupport = true
+        }
+      }
+    }
+  }
+
+  local workspace_folder = home .. "/.workspace/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+  local config = {
+    flags = {
+      allow_incremental_sync = true,
+    };
+    capabilities = capabilities,
+  }
+
+  config.on_init = function(client, _)
+    client.notify('workspace/didChangeConfiguration', { settings = config.settings })
+  end
   config.on_attach = function(client, buffnr)
+    require'jdtls.setup'.add_commands()
     require'lsp.keymap'.set_keymaps(client, buffnr)
+    require'lspsaga'.init_lsp_saga()
   end
 
-  config.cmd = { 'java-lsp' }
+  local extendedClientCapabilities = require'jdtls'.extendedClientCapabilities
+  extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+  config.init_options = {
+    -- bundles = bundles;
+    extendedClientCapabilities = extendedClientCapabilities;
+  }
+
+  config.cmd = { 'java-lsp', workspace_folder }
   require('jdtls').start_or_attach(config)
 end
 
--- plugins/nvim/.config/nvim/lua/plugins/lspsaga.lua
+++ plugins/nvim/.config/nvim/lua/plugins/lspsaga.lua
@@ -1,2 +1,8 @@
 local saga = require 'lspsaga'
-saga.init_lsp_saga()
+
+local server_map = {}
+server_map['jdt.ls'] = {'java'}
+
+saga.init_lsp_saga({
+  server_filetype_map = server_map 
+})
