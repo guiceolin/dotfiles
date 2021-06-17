89b2b6b67833c5bcbb203399bdc51692434194ee
Guilherme Ceolin <guiceolin@gmail.com>
2021-06-17 14:46:25 -0300

    (nvim) fix lspsaga configs

--- plugins/nvim/.config/nvim/lua/plugins/lspsaga.lua
+++ plugins/nvim/.config/nvim/lua/plugins/lspsaga.lua
@@ -4,5 +4,11 @@ local server_map = {}
 server_map['jdt.ls'] = {'java'}
 
 saga.init_lsp_saga({
-  server_filetype_map = server_map 
+  server_filetype_map = server_map,
+  code_action_prompt = {
+   enable = true,
+   sign = true,
+   sign_priority = 20,
+   virtual_text = false,
+ },
 })
