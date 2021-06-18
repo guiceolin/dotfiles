8eaadcd3f8b817e758e8ab048cfc197bff7ff33e
Guilherme Ceolin <guiceolin@gmail.com>
2021-06-18 14:03:20 -0300

    (pyenv) update pyenv plugins

--- plugins/pyenv/install.sh
+++ plugins/pyenv/install.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
-git clone https://github.com/s1341/pyenv-alias.git $(pyenv root)/plugins/pyenv-alias
+git clone https://github.com/s1341/pyenv-alias.git $HOME/.pyenv/plugins/pyenv-alias
 
--- plugins/pyenv/pyenv.plugin.zsh
+++ plugins/pyenv/pyenv.plugin.zsh
@@ -3,5 +3,5 @@ if [ -z "$PYENV_ROOT" ]; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
 
-  eval "$(pyenv init -)"
+  eval "$(pyenv init --path)"
 fi
