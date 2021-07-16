f9d245d7a4b3c695fac660720a956e39783fe422
Guilherme Ceolin <guiceolin@gmail.com>
2021-07-16 13:43:36 -0300

    (nvim) Add bqf

--- plugins/nvim/.config/nvim/lua/plugins.lua
+++ plugins/nvim/.config/nvim/lua/plugins.lua
@@ -3,6 +3,8 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
+  use 'kevinhwang91/nvim-bqf'
+
   use 'neovim/nvim-lspconfig'
   use 'glepnir/lspsaga.nvim'
   use {
