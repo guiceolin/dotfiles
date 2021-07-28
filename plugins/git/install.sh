dcec28f6582b07f449d466e331677f1aa4308710
Guilherme Ceolin <guiceolin@gmail.com>
2021-07-28 12:07:33 -0300

    (git) use delta instead diff-so-fancy

--- plugins/git/install.sh
+++ plugins/git/install.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/env zsh 
+#!/usr/bin/env zsh
 
 set -x
+#
+# ln -sf $DOTFILES/plugins/git/.config/git ~/.config
+# mkdir -p $DOTFILES/plugins/git/bin
+# pushd $DOTFILES/plugins/git/bin
+# curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy > diff-so-fancy
+# chmod +x diff-so-fancy
+# popd
+#
+# ln -sf $DOTFILES/plugins/git/bin/diff-so-fancy ~/.local/bin/
 
-ln -sf $DOTFILES/plugins/git/.config/git ~/.config
-mkdir -p $DOTFILES/plugins/git/bin
-pushd $DOTFILES/plugins/git/bin
-curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy > diff-so-fancy
-chmod +x diff-so-fancy
-popd
-
-ln -sf $DOTFILES/plugins/git/bin/diff-so-fancy ~/.local/bin/
+# TODO: install delta
