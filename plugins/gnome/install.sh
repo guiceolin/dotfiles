38f7a84bc98ab1d3375d4613633875637352ea6f
Guilherme Ceolin <guiceolin@gmail.com>
2021-08-03 15:16:30 -0300

    (gnome) Add cedilla fix

--- plugins/gnome/install.sh
+++ plugins/gnome/install.sh
@@ -10,3 +10,5 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 64
 gsettings set org.gnome.shell.extensions.dash-to-dock unity-backlit-items true
+
+export GTK_IM_MODULE=cedilla
