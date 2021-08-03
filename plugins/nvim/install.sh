3879eb7d1cc7aa9a89534d9d98e187701fb97787
Guilherme Ceolin <guiceolin@gmail.com>
2021-08-03 15:07:11 -0300

    (nvim) fix lua lsp install

--- plugins/nvim/install.sh
+++ plugins/nvim/install.sh
@@ -9,17 +9,17 @@ ln -sf $DOTFILES/plugins/nvim/.config/nvim ~/.config
 # # Install python 3
 # pyenv install -s 3.6.6
 # pyenv global 3.6.6
-# 
+#
 # pip install --upgrade pip
 # pip install virtualenv
-# 
+#
 # NVIM_VIRTUALENV=~/.local/.venv
-# 
+#
 # mkdir -p $NVIM_VIRTUALENV
 # virtualenv $NVIM_VIRTUALENV
-# 
+#
 # $NVIM_VIRTUALENV/bin/pip install neovim flake8 black
-# 
+#
 # ln -s $HOME/.local/.venv/bin/flake8 ~/.local/bin/flake8
 
 PACKER_PATH=~/.local/share/nvim/site/pack/packer/start/packer.nvim
@@ -29,7 +29,7 @@ if [ ! -d $PACKER_PATH ]; then
 fi
 
 # Install LSPs
-if [ ! -d $DOTFILES/plugins/nvim/lsp ]; then 
+if [ ! -d $DOTFILES/plugins/nvim/lsp ]; then
   mkdir -p $DOTFILES/plugins/nvim/lsp
 fi
 
@@ -47,7 +47,7 @@ if [ ! -d "./lua-language-server" ]; then
 
   git submodule update --init --recursive
   cd 3rd/luamake
-  ninja -f ninja/macos.ninja
+  compile/install.sh
   cd ../..
   ./3rd/luamake/luamake rebuild
   cd ..
