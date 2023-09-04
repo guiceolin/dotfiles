b800dd9084a58ffb6f24ac86cd85d64ba07af9a2
Guilherme Ceolin <guiceolin@gmail.com>
2023-09-04 16:02:05 -0300

    (nvim) treesitter cleanup

--- plugins/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ plugins/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,18 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  build = function()
-        require("nvim-treesitter.install").update()
-    end,
-    config = function()
-      local opts = {
-        ensure_installed = { "java", "javascript", "python", "lua", "vim" },
-        highlight = {
-          enable = true,
-        },
-        indent = {
-          enable = true,
-        },
-      }
-      require('nvim-treesitter.configs').setup(opts)
-    end
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    local opts = {
+      ensure_installed = { "java", "javascript", "python", "lua", "vim" },
+      sync_install = false,
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+    }
+    require('nvim-treesitter.configs').setup(opts)
+  end
 }
