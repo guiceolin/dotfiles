601af55cb6ea2692def880e1983952ff87da8779
Guilherme Ceolin <guiceolin@gmail.com>
2024-01-05 18:11:48 -0300

    (nvim) new telescope keymaps keymap

--- plugins/nvim/.config/nvim/lua/configs/navigation.lua
+++ plugins/nvim/.config/nvim/lua/configs/navigation.lua
@@ -4,5 +4,6 @@ nnoremap <leader>p :NvimTreeToggle<CR>
 nnoremap <leader>t <cmd>lua require('telescope.builtin').find_files()<cr>
 nnoremap <leader>/ <cmd>lua require('telescope.builtin').live_grep()<cr>
 nnoremap <leader>b <cmd>lua require('telescope.builtin').buffers()<cr>
+nnoremap ? <cmd>lua require('telescope.builtin').keymaps()<cr>
 ]]
 
