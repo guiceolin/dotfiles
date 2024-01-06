ffb4e613728967b184453881731f7c1da7e5c1a9
Guilherme Ceolin <guiceolin@gmail.com>
2024-01-05 21:26:21 -0300

    (nvim) add whichkey

--- plugins/nvim/.config/nvim/lua/plugins/init.lua
+++ plugins/nvim/.config/nvim/lua/plugins/init.lua
@@ -7,7 +7,15 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
   {
-  'lewis6991/gitsigns.nvim',
-    dependencies  = { 'nvim-lua/plenary.nvim' },
+    'lewis6991/gitsigns.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
   }
 }
