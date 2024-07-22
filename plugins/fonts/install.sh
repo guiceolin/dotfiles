e412eab255078929c7120e27cbb82e0e6738b16b
Guilherme Ceolin <guiceolin@gmail.com>
2024-07-22 09:47:32 -0300

    (font) new fonts

--- plugins/fonts/install.sh
+++ plugins/fonts/install.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 # Install Mononoki font
-mkdir /tmp/mononoki && cd /tmp/mononoki
+mkdir /tmp/fonts
+cd /tmp/fonts
 
-LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/ryanoasis/nerd-fonts/releases/latest|jq '.tag_name'|sed 's/"//g')
-wget "https://github.com/ryanoasis/nerd-fonts/releases/download/$LATEST_VERSION/mononoki.zip"
+# loop throught array
+for font_name in mononoki CascadiaMono
+do
+	curl -OL "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
+	unzip "$font_name".zip
+done 
 
-unzip mononoki.zip
 # TODO Check OS before cp fonts
 mkdir -p ~/Library/Fonts/
 cp ./*.ttf ~/Library/Fonts/
@@ -13,4 +17,4 @@ mkdir -p ~/.local/share/fonts/
 cp ./*.ttf ~/.local/share/fonts/
 sudo fc-cache -vf ~/.local/share/fonts
 
-cd - && rm -rf /tmp/mononoki
+cd - && rm -rf /tmp/fonts
