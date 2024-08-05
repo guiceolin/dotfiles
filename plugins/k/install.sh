8172edfb9a131e8b876425b7d69f5f488ce31362
Guilherme Ceolin <guiceolin@gmail.com>
2024-08-05 11:33:19 -0300

    remove unused plugins

--- plugins/k/install.sh
+++
@@ -1,7 +0,0 @@
-which -s git
-if [[ $? != 0 ]] ; then
-  echo -n '> To install k plugin, you need git.'
-  echo -n '> Skipping k plugin install.'
-else
-  git clone https://github.com/supercrabtree/k.git
-fi
--- plugins/k/k.plugin.zsh
+++
@@ -1,4 +0,0 @@
-if [[ -f "${0:h}/k/k.sh" ]] ; then
-  source "${0:h}/k/k.sh"
-fi
-
