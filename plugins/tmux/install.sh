c685973005577e7f38626b8855ea27c6c16fa7c8
Guilherme do Amaral Ceolin <guilherme.ceolin@BRSPPM-KCF4TVFY97.local>
2024-10-01 15:17:56 -0300

    (tmux) use brew tmux

--- plugins/tmux/install.sh
+++ plugins/tmux/install.sh
@@ -1,23 +1,5 @@
 #!/usr/bin/env bash
 
-# Install Tmux
-
-if ! type "tmux" > /dev/null; then
-  printf "Installing Tmux from source...\n"
-  rm -fr /tmp/tmux
-  git clone https://github.com/tmux/tmux.git /tmp/tmux
-  cd /tmp/tmux
-  sh autogen.sh
-  ./configure && make
-  cp tmux ~/.local/bin
-  cd -
-  rm -fr /tmp/tmux
-else
-  printf "Tmux already installed, skiping install...\n"
-fi
-
-# Install tmux.conf
-
 if [[ -f ~/.tmux.conf ]]; then
   mv ~/.tmux.conf ~/.tmux.conf.old
 fi
