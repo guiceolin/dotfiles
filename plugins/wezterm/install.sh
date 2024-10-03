b1dcd3699959b90bebd837345f79661cd951944c
Guilherme Ceolin <guiceolin@gmail.com>
2024-10-03 09:41:55 -0300

    (wezterm) use new link to config

--- plugins/wezterm/install.sh
+++ plugins/wezterm/install.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh 
+autoload -Uz _dotfiles_link_to_config
 
-mkdir -p ~/.config
-if [[ -f ~/.config/wezterm ]]; then
-  mv ~/.config/wezterm ~/.config/wezterm.old
-fi
-ln -sf $DOTFILES/plugins/wezterm ~/.config
+_dotfiles_link_to_config wezterm
+# 
+# mkdir -p ~/.config
+# if [[ -f ~/.config/wezterm ]]; then
+#   mv ~/.config/wezterm ~/.config/wezterm.old
+# fi
+# ln -sf $DOTFILES/plugins/wezterm ~/.config
