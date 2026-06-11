a19d07ff910ded0268a0ba4fd087ce27fa6e9d49
Guilherme Ceolin <guiceolin@gmail.com>
2026-06-11 14:49:38 -0300

    feat(fnm): split PATH setup into fnm.env.zsh, remove lazy_load

---
+++ plugins/fnm/fnm.env.zsh
@@ -0,0 +1,2 @@
+export FNM_PATH="$HOME/.local/share/fnm"
+path=($FNM_PATH $FNM_PATH/aliases/default/bin $path)
--- plugins/fnm/fnm.plugin.zsh
+++ plugins/fnm/fnm.plugin.zsh
@@ -1,7 +1,2 @@
-(( $+functions[lazy_load] )) || source "$DOTFILES/lib/lazy_load.zsh"
-
-lazy_load 'fnm' 'node' 'npm' 'npx' <<- 'EOF'
-  FNM_PATH="$HOME/.local/share/fnm"
-  export PATH="$FNM_PATH:$PATH"
-  eval "$(fnm env --shell zsh)"
-EOF
+(( $+commands[fnm] )) || return
+eval "$(fnm env --shell zsh)"
